1c479c68a6eb06da9a19f7a7382db1e54c3bd5a2
Alenka <If you don't already have an SSH key, you must generate a new SSH key. If you're unsure whether you already have an SSH key, check for existing keys>
2021-05-28 03:00:16 +0300

    Изменение в 5 задании

--- js/task-05.js
+++ js/task-05.js
@@ -11,8 +11,9 @@ const nameUser = document.querySelector('#name-output');
 name.addEventListener('input',onInputChange);
 
 function onInputChange(event){
-    nameUser.textContent=event.currentTarget.value;
-    if(!event.currentTarget.value){
+    const value = event.currentTarget.value.trim();
+    nameUser.textContent=value;
+    if(!value){
         nameUser.textContent = 'незнакомец';
     };
 };
